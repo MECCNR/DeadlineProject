51fea59ca3f3a2eb99e34681421138bb7e8d09d0
MECCNR <e.migalckin@yandex.ru>
2022-10-21 11:48:18 +0900

    new entrypoint

---
+++ repoclone/src/test/java/VerifyPage.java
@@ -0,0 +1,12 @@
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerifyPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+
+    public VerifyPage() {
+        codeField.shouldBe(visible);
+    }
+}
